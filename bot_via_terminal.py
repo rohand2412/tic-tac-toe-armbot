005dc0247c37ec03ed6fa856b4fe663f0414de73
Rohan Dugad <rohand2468@gmail.com>
2022-03-19 14:44:39 -0400

    Moved high level api calls to separate file

---
+++ bot_via_terminal.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Script to interact with the bot via the terminal"""
+
+from tic_tac_toe_bot import TicTacToeBot, BoardStatus
+import enum
+
+class State(enum.Enum):
+    COMPUTER_TURN = 0
+    HUMAN_TURN = 1
+
+def main() -> None:
+    bot = TicTacToeBot()
+    state = State.HUMAN_TURN
+    while bot.is_end_of_game() == BoardStatus.UNFINISHED:
+        if state == State.HUMAN_TURN:
+            bot.print_board()
+            row = int(input("Row: ")) - 1
+            col = int(input("Col: ")) - 1
+            print("")
+
+            if bot.play_opponent(row, col):
+                state = State.COMPUTER_TURN
+
+        elif state == State.COMPUTER_TURN:
+            bot.play()
+            state = State.HUMAN_TURN
+
+    print(bot.is_end_of_game().name)
+
+if __name__ == '__main__':
+    main()
